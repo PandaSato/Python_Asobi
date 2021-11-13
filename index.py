d1cb3cd71b83c698b1bd5596e81d45bd091162a9
PandaSato <pandasato004@gmail.com>
2021-11-13 13:30:29 +0000

    p_snippet_init

---
+++ index.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# QTableWidget Example @pythonspot.com
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+import sys
+
+#자동완성을 위해서 만들어놓은 오브젝트 리스트들..
+#실제로 쓸 필요는 없음.
+QtObjList = [QWidget, 
+             QVBoxLayout, QGridLayout,
+             QLineEdit,QPushButton,QTextEdit,QTextBrowser]
+
+
+class Display(QWidget):
+    def __init__(self):
+        super(Display,self).__init__()
+        
+        ## Make UI
+        self.resize(400,400)
+        self.layout = QVBoxLayout()
+        self.example = QLineEdit("Hello World")
+        self.button = QPushButton("Hello World!")
+        self.layout.addWidget(self.example)
+        self.layout.addWidget(self.button)
+        self.setLayout(self.layout)
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    display = Display()
+    display.show()
+    app.exec_()
---
+++ snippet1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# QTableWidget Example @pythonspot.com
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+import sys,time,os,random
+
+
+
+##특정 객체명이 가지고 있는 메소드 혹은 요소를 찾아본다.
+# ex) check(QLayout,'set')
+def check(*args):
+    objname=args[0]
+    s=""
+    if len(args)>1: s=args[1]    
+    l = dir(objname)
+    result = []
+    for line in l:
+        if line.count(s)>0: result.append(line)
+    return result
