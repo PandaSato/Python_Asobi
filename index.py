8bb3dd72c6ffa8d9961e23bec171cf77bbc26f4d
pandasato004@gmail.com <pandasato004@gmail.com>
2021-11-14 13:55:18 +0000

    drawSquare_Function

--- index.py
+++ index.py
@@ -13,6 +13,8 @@ QtObjList = [QWidget,
 #test
 
 
+#Genesis class is Base class of window. You can define your own windows as its base as Genesis.
+#It has useful functions, and cleaned up messy things.
 
 class Genesis(QWidget):
     def __init__(self):
@@ -24,7 +26,7 @@ class Genesis(QWidget):
         ###Paint Setting
         self.painter = QPainter()
         self.color = 0xFFFFFF
-        self.font = ['MS Mincho',20]
+        self.font = ['MS Gothic',20]
     def paintEvent(self, event):
 
         self.painter.begin(self)
@@ -47,6 +49,18 @@ class Genesis(QWidget):
         self.painter.setPen(QColor(self.color))
         self.painter.setFont(QFont(self.font[0],self.font[1]))
         self.painter.drawText(x,y, QString(unicode(s, 'utf-8')))        
+    def drawSquare(self, x, y, size):
+        
+        color = QColor(self.color)
+        self.painter.fillRect(x + 1, y + 1, size - 2, size - 2, color)
+
+        self.painter.setPen(color.light())
+        self.painter.drawLine(x, y + size - 1, x, y)
+        self.painter.drawLine(x, y, x + size - 1, y)
+
+        self.painter.setPen(color.dark())
+        self.painter.drawLine(x + 1, y + size - 1, x + size - 1, y + size - 1)
+        self.painter.drawLine(x + size - 1, y + size - 1, x + size - 1, y + 1)
 class MainWindow(Genesis):
     def __init__(self):
         super(MainWindow,self).__init__()
@@ -59,7 +73,12 @@ class MainWindow(Genesis):
     def initUI(self):
         self.resize(400,400)
     def drawEvent(self, event):
+        self.color = 0xFFFFFF
         self.drawBunsho(10, 50, '直接（ちょくせつ）に聞（き）いてください。')
+        self.color = 0xAA00AA
+        for dx in range(0,5):
+            for dy in range(0,5):
+                self.drawSquare(5+dx*10,60+dy*10,10)
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mwindow = MainWindow()
