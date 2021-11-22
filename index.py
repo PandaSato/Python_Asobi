96ebd53a7b0ed2c153119a40f09df2562c399669
pandasato004@gmail.com <pandasato004@gmail.com>
2021-11-22 13:59:28 +0000

    PLF_memo_Function_Updated

--- index.py
+++ index.py
@@ -8,24 +8,54 @@ libs={"QtGui":QtGui,"QtCore":QtCore, "QtWidgets":QtWidgets, "os":os,"time":time,
 
 global SelectedLibrary
 
-
+directoryName = "PLF-MEMO"
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-        global SelectedLibrary
+        global SelectedLibrary, currentMemoTo
         SelectedLibrary=QtGui
         self.initUI()
+    def loadMEMO(self,name):
+        self.memoLabel.setText("MEMO on "+name)
+        path = directoryName+'/'+name+'.txt'
+        if len(glob.glob(path))>0:
+            f = open(path,'r')
+            self.memoEdit.setPlainText(''.join(f.readlines()))
+        else:
+            self.memoEdit.setPlainText("")
+    def editMemo(self):
+        if self.memoEdit.toPlainText()!="":
+            name = self.memoLabel.text().split(' ')[2]
+            path = directoryName+'/'+name+'.txt'
+
+            if len(glob.glob(directoryName))<1:
+                os.system('mkdir '+directoryName)
+            if len(glob.glob(path))<1:
+                os.system('touch '+path)
+            f = open(path,'w')
+            f.write(self.memoEdit.toPlainText())
+        
+        
     def getMemo(self):
-        printLog(self.list3.list.currentItem().text())
+        try:
+            libname = self.list1.list.currentItem().text()
+            objname = self.list2.list.currentItem().text()
+            methodname = self.list3.list.currentItem().text()
+            #self.memoLabel.setText("MEMO on "+libname+']'+objname+']'+methodname)
+            self.loadMEMO(libname+']'+objname+']'+methodname)
+        except:
+            pass
     def selectLibrary(self):
         try:
             t = self.list1.list.currentItem()
             global SelectedLibrary
-            SelectedLibrary=libs[str(t.text())]
+            SelectedLibrary=libs[t.text()]
             self.list2.WholeList=dir(SelectedLibrary)
             self.list2.search.setText("")
             self.list3.search.setText("")
+            #self.memoLabel.setText("MEMO on "+t.text())
+            self.loadMEMO(t.text())
             self.list2.listUpdate()
         except:
             pass
@@ -33,8 +63,12 @@ class MainWindow(QWidget):
         try:
             t = self.list2.list.currentItem()
             global SelectedLibrary
-            self.list3.WholeList=dir(getattr(SelectedLibrary,str(t.text())))
+            self.list3.WholeList=dir(getattr(SelectedLibrary,t.text()))
             self.list3.listUpdate()
+            libname = self.list1.list.currentItem().text()
+            objname = t.text()
+            #self.memoLabel.setText("MEMO on "+libname+']'+objname)
+            self.loadMEMO(libname+']'+objname)
         except:
             pass
     def initUI(self):
@@ -50,21 +84,19 @@ class MainWindow(QWidget):
         self.list2=SearchList("Classes")
         self.list2.list.currentItemChanged.connect(self.selectObject)        
         self.list3=SearchList("Components")
-        self.list3.list.itemDoubleClicked.connect(self.getMemo)
+        self.list3.list.currentItemChanged.connect(self.getMemo)
         self.layout.addWidget(self.list1)
         self.layout.addWidget(self.list2)
         self.layout.addWidget(self.list3)
 
         self.mainLayout = QVBoxLayout()
         self.memoEdit = QTextEdit()
+        self.memoLabel = QLabel("MEMO")
+        self.memoEdit.textChanged.connect(self.editMemo)
         self.mainLayout.addLayout(self.layout)
+        self.mainLayout.addWidget(self.memoLabel)
         self.mainLayout.addWidget(self.memoEdit)
         self.setLayout(self.mainLayout)
-    def keyPressEvent(self,event):
-        if event.key() == QtCore.Qt.Key_Z:
-            self.memoEdit.hide()
-        if event.key() == QtCore.Qt.Key_X:
-            self.memoEdit.show()
             
 
 if __name__ == '__main__':
