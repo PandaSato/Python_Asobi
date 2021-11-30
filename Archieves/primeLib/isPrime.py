d05ac9bee79a70a229a513df3d22d0fa1e029b7f
pandasato004@gmail.com <pandasato004@gmail.com>
2021-11-30 14:14:05 +0000

    Made_PrimeLib

---
+++ Archieves/primeLib/isPrime.py
@@ -0,0 +1,23 @@
+
+from makePrimeList import folder
+import sys,glob,os,pickle
+
+if __name__=='__main__':
+    p = int(sys.argv[1])
+    nums = [int(os.path.basename(x)) for x in glob.glob(folder+'/*')]
+    nums.sort()
+    select = ''
+    for num in nums:
+        if p<=num:
+            select = folder+'/'+str(num)
+            break
+    if select=='':
+        print(str(p) + ' is too big')
+        sys.exit()
+    file = open(select,'rb')
+    plist = [2,3]+pickle.load(file)
+    if p in plist:
+        print(str(p) + ' is a prime')
+    else:
+        print(str(p) + ' is not a prime')
+    
---
+++ Archieves/primeLib/makePrimeList.py
@@ -0,0 +1,53 @@
+import pickle,os,glob
+folder = 'PrimeNumbers'
+
+
+##Using plist(prime List), from sp(start Prime), make new Prime List that has length of cnt(count)
+def makePrimeList(plist, sp, cnt):
+    
+    ##initialization
+
+    if plist==[]:
+        plist=[2]
+    new_plist = []
+    if sp<=1:
+        new_plist.append(2)
+    sp = max(1,sp)
+    if sp%2==0:
+        sp+=1
+    
+    #make new prime list
+    while cnt>0:
+        sp+=2
+        sqrt = pow(sp,0.5)
+        isPrime = True
+        for p in plist:
+            if p>sqrt:
+                break
+            if sp%p==0:
+                isPrime = False
+                break
+        if isPrime:
+            plist.append(sp)
+            new_plist.append(sp)
+            cnt-=1
+    return new_plist
+        
+if __name__=='__main__':
+    
+    ##initialize prime list
+    if len(glob.glob(folder))==0:
+        os.system("mkdir "+folder)
+    plist=[2,3]
+    names = glob.glob(folder+'/*')
+    names.sort(key = lambda x: int(os.path.basename(x)))
+    for primefile in names:
+        plist+=pickle.load(open(primefile,'rb'))
+    
+    while True:
+        list = makePrimeList(plist,plist[-1],100000)
+        name = list[-1]
+        path = folder+'/'+str(name)
+        os.system('touch '+path)
+        file = open(path,'wb')
+        pickle.dump(list,file)
